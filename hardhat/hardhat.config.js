4923ef82c88bc3c56de281a6bd4e3dbe1a75b202
abhinna1 <manandharabhinna@gmail.com>
2023-05-09 22:28:19 +0545

    config update

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,14 +5,14 @@ require("@nomicfoundation/hardhat-toolbox");
 //setting up the hardhat network to use the local network
 module.exports = {
   solidity: "0.8.18",
-  defaultNetwork: "hardhat",
+  defaultNetwork: "localhost",
   networks: {
     hardhat: {
       chainId: 1337,
     },
     localhost: {
       url: "HTTP://127.0.0.1:7545",
-      privateKey: "a93e56c63dc65a137809c7d8c2e12e2eae62fe43d99be3c20a1224b579101c29"
+      privateKey: ""
     }
   },
 
